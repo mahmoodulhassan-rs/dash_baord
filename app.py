c9e4608451aa9b9b6f8b9cc0c74adcf8d2850cb4
mahmoodulhassan-rs <86703923+mahmoodulhassan-rs@users.noreply.github.com>
2022-07-23 20:52:59 +0500

    Update app.py

--- app.py
+++ app.py
@@ -55,7 +55,8 @@ f1 = go.Figure(
     layout = {"xaxis": {"title": "Weeks"}, "yaxis": {"title": "Numbers(Tets Passed, Tests Failed, Tests Planned)"}, "title": "Weekly Statistics"}
 )
 
-app.layout = html.Div(style = {
+app.layout = app.layout = html.H1('The time is: ' + str(datetime.datetime.now())),
+html.Div(style = {
   'backgroundColor': '#FFFFFF'
 }, children = [
     html.H1(

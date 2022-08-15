658204da7e4755d11b4dfdf9afbf3b169967723c
Mahmood Ul Hassan <mahmood.hassan@rapidsilicon.com>
2022-08-15 20:42:39 +0500

    new

--- app.py
+++ app.py
@@ -83,10 +83,10 @@ legend=dict(
     x=0.00,
 ))
 
-arr =[158,158,158,158,158]
-aarr= [21,35,39,56,65]
-arr_f= [0,3,3,0,4]
-arr_x=[1,2,3,4,5,6,7]
+arr =[158,158,158,158,158,158]
+aarr= [21,35,39,56,65,87]
+arr_f= [0,3,3,0,4,9]
+arr_x=[1,2,3,4,5,6,7,8,9]
 f1 = go.Figure(
     data = [
         go.Scatter(x=arr_x,y=arr, name="Tests Planned",line=dict(color="#00BFFF")),

6046cfc1745a7f7e1b0f5fe147f8ed86313d52ee
mahmoodulhassan-rs <86703923+mahmoodulhassan-rs@users.noreply.github.com>
2022-09-19 11:40:12 +0500

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ server = app.server
 fig = px.pie(df_grp_dash, values='Numbers', names='Category',color = "Category",hole=.2,color_discrete_map={'Tests Passed':'#66CDAA',
                                  'Tests Under Development':'#00BFFF',                                                                           
                                  'Tests Failed':'#CD5C5C'})
-fig.update_layout(title={'text': '<b>Pie Chart SoC</b>','y':1,
+fig.update_layout(title={'text': '<b>Pie Chart SoC</b>','y':0.8,
         'x':0.5,
         'xanchor': 'center',
         'yanchor': 'top'},

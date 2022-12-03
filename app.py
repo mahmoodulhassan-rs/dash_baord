bffca7a8f8c39f505a08f86180555249362e3773
Mahmood Ul Hassan <mahmood.hassan@rapidsilicon.com>
2022-12-03 18:03:00 +0500

    update

--- app.py
+++ app.py
@@ -11,13 +11,13 @@ df_states = pd.read_csv('css_stats.csv')
 df_time_lg = pd.read_csv('time.csv')
 
 sram_df_grp_dash=pd.read_csv('sram_pie_stats_dash.csv')
-sram_df_table = pd.read_csv('sram_css_stats_dash.csv')
-sram_df_states = pd.read_csv('sram_css_stats.csv')
+sram_df_table = pd.read_csv('sram_css_states_dash.csv')
+sram_df_states = pd.read_csv('sram_css_states.csv')
 sram_df_time_lg = pd.read_csv('sram_time.csv')
 
-fcb_df_grp_dash=pd.read_csv('fcb_pie_stats_dash.csv')
-fcb_df_table = pd.read_csv('fcb_css_stats_dash.csv')
-fcb_df_states = pd.read_csv('fcb_css_stats.csv')
+fcb_df_grp_dash=pd.read_csv('fcb_pie_states_dash.csv')
+fcb_df_table = pd.read_csv('fcb_css_states_dash.csv')
+fcb_df_states = pd.read_csv('fcb_css_states.csv')
 fcb_df_time_lg = pd.read_csv('fcb_time.csv')
 
 

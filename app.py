11b7a46ba1487af3cee270621655d8327d042fb2
Mahmood Ul Hassan <mahmood.hassan@rapidsilicon.com>
2022-12-08 12:25:22 +0500

    new

--- app.py
+++ app.py
@@ -699,7 +699,7 @@ app.layout = html.Div((
      html.Div([
          html.Img(src='assets/index_2.png',
             style={
-                'height': '60%',
+                'height': '80%',
                 'width': '40%'
             })
 ], style={'textAlign': 'center'}),
